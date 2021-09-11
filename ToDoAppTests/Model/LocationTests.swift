8d22df111138fae29b1a8edf71c8e2f739156817
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2021-09-10 23:30:19 -0500

    location

---
+++ ToDoAppTests/Model/LocationTests.swift
@@ -0,0 +1,22 @@
+//
+//  LocationTests.swift
+//  ToDoAppTests
+//
+//  Created by Evgeniy on 9/10/21.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class LocationTests: XCTestCase {
+    
+    override class func setUp() {
+        
+    }
+    
+    override class func tearDown() {
+        
+    }
+
+}
